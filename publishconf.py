d74bc52f54e93f7dbaf557cfe9268a90a234b869
Héctor Louzao <louzaoh@gmail.com>
2025-09-21 21:18:19 +0200

    fix blank page on first post

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://hhlp.github.io/blog/'
+SITEURL = 'https://hhlp.github.io/blog'
+RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
